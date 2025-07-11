9a60134c71c703751a0da7a9e94f9ae5d609f7cf
Gabriel <gabriel.ortega.t@outlook.com>
2025-07-11 15:27:44 -0400

    mejoras a la funcion cuadratica

--- limite de una funcion/main.cpp
+++ limite de una funcion/main.cpp
@@ -6,6 +6,8 @@ float respuesta_numerica;
 char respuestasn = 'n';
 float multiplicador_x;
 float sumador_x;
+int operacion;        
+float exponente;
 
 int main()
 {
@@ -18,29 +20,37 @@ cin >> respuesta_numerica;
 // Cuadratica
 if (respuesta_numerica== 1) {
     cout << "Funcion Cuadratica x^2" << endl;
-    cout << "¿Desea multiplicar la funcion x^2? s/n" << endl;
-    cin >> respuestasn;
-    if (respuestasn == 's' ){
-        cout << "Por cuanto desea multiplicar x^2: ";
+    cout << "¿Qué operación desea realizar?" << endl;
+    cout << "1. Elevar a un número" << endl;
+    cout << "2. Multiplicar por un número" << endl;
+    cout << "3. Sumar un número" << endl;
+    cin >> operacion;
+
+    if (operacion == 1) {
+
+        cout << "¿A qué exponente desea elevar x^2? ";
+        cin >> exponente;
+        cout << "Su funcion es: x^(" << 2 * exponente << ")" << endl;
+        cout << "Escriba un valor para x: ";
+        cin >> valor_x;
+        cout << "El resultado es: " << pow(valor_x, 2 * exponente) << endl;
+    } else if (operacion == 2) {
+        cout << "¿Por cuánto desea multiplicar x^2? ";
         cin >> multiplicador_x;
         cout << "Su funcion es: " << multiplicador_x << " * x^2" << endl;
-        cout << "¿Desea sumar la funcion " << multiplicador_x << " * x^2? s/n" << endl;
-        cin >> respuestasn;
-        if (respuestasn == 's' ){
-             cout << "Por cuanto desea sumar la funcion " << multiplicador_x << " * x^2"  << endl;
-             cin >> sumador_x;
-             cout << "Su funcion es: " << multiplicador_x << " * x^2" << " + (" << sumador_x <<")";
-        }
-
-    } //
-    //cout << "Escriba un valor para x" << endl;
-    //cin >> valor_x;
-    //cout << 
-    
-    
-    
-    // cout << "Su funcion es " << valor_x << "^2" << endl;
-    // cout << "El limite de la funcion es: " << (valor_x*valor_x);
+        cout << "Escriba un valor para x: ";
+        cin >> valor_x;
+        cout << "El resultado es: " << multiplicador_x * pow(valor_x, 2) << endl;
+    } else if (operacion == 3) {
+        cout << "¿Qué número desea sumar a x^2? ";
+        cin >> sumador_x;
+        cout << "Su funcion es: x^2 + " << sumador_x << endl;
+        cout << "Escriba un valor para x: ";
+        cin >> valor_x;
+        cout << "El resultado es: " << pow(valor_x, 2) + sumador_x << endl;
+    } else {
+        cout << "Opción no válida." << endl;
+    }
 };
 
 // lineal
